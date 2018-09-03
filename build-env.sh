8a3d1c9b9f5885cd46dd9ddbc9faebdc224327e4
Nicolas Ménant <menant.nicolas@gmail.com>
2018-09-03 17:19:38 +0200

    update build script

--- build-env.sh
+++ build-env.sh
@@ -42,6 +42,48 @@ elif [[ "$platform" == 'Darwin' ]]; then
 fi
 
 ##
+## SETUP MINISHIFT
+##
+
+##
+## Need to identify the platform to make sure we can install Minishift
+## installation guide is here: ## update accordingly:  https://docs.okd.io/latest/minishift/getting-started/installing.html
+##
+
+echo "#################################################"
+echo "SETTING UP MINISHIFT"
+echo "#################################################"
+
+
+if [[ "$platform" == 'Linux' ]]; then
+    #echo "Linux deployment is not automated yet, please set it up yourself: https://docs.okd.io/latest/minishift/using/run-against-an-existing-machine.html#configuring-existing-remote-machine"
+    mkdir minishift 
+    wget https://github.com/minishift/minishift/releases/download/v1.23.0/minishift-1.23.0-linux-amd64.tgz
+    tar zxf minishift-1.23.0-linux-amd64.tgz -C minishift/
+    mv minishift/minishift-1.23.0-linux-amd64/minishift /usr/local/bin/
+    sudo firewall-cmd --permanent --add-port 2376/tcp --add-port 8443/tcp --add-port 80/tcp
+    dockernet=`sudo docker network inspect -f "{{range .IPAM.Config }}{{ .Subnet }}{{end}}" bridge`
+    sudo firewall-cmd --permanent --new-zone minishift
+    sudo firewall-cmd --permanent --zone minishift --add-source $dockernet
+    sudo firewall-cmd --permanent --zone minishift --add-port 53/udp --add-port 8053/udp
+    sudo firewall-cmd --reload
+    read -p 'VM IP: ' serverip
+    minishift addons install --defaults
+    minishift addons enable admin-user
+    minishift addon apply admin-user
+    minishift start --vm-driver generic --remote-ipaddress $serverip --remote-ssh-user $USER --remote-ssh-key $HOME/.ssh/id_rsa
+elif [[ "$platform" == 'MACOSX' ]]; then
+    echo "Installing Minishift on a MACOSX platform" 
+    brew cask install minishift
+
+    minishift addons install --defaults
+    minishift addons enable admin-user
+    minishift addon apply admin-user
+    minishift start
+fi
+
+
+##
 ## Retrieve the containers' volumes from AWS S3
 ##
 
@@ -72,43 +114,6 @@ if [[ "$output" == '' ]]; then
    docker network create --subnet=172.18.0.0/16 ci-cd-docker-net
 fi
 
-##
-## SETUP MINISHIFT
-##
-
-##
-## Need to identify the platform to make sure we can install Minishift
-## installation guide is here: ## update accordingly:  https://docs.okd.io/latest/minishift/getting-started/installing.html
-##
-
-echo "#################################################"
-echo "SETTING UP MINISHIFT"
-echo "#################################################"
-
-
-if [[ "$platform" == 'Linux' ]]; then
-    #echo "Linux deployment is not automated yet, please set it up yourself: https://docs.okd.io/latest/minishift/using/run-against-an-existing-machine.html#configuring-existing-remote-machine"
-    mkdir minishift 
-    wget https://github.com/minishift/minishift/releases/download/v1.23.0/minishift-1.23.0-linux-amd64.tgz
-    tar zxf minishift-1.23.0-linux-amd64.tgz -C minishift/
-    mv minishift/minishift-1.23.0-linux-amd64/minishift /usr/local/bin/
-    sudo firewall-cmd --permanent --add-port 2376/tcp --add-port 8443/tcp --add-port 80/tcp
-    dockernet=`sudo docker network inspect -f "{{range .IPAM.Config }}{{ .Subnet }}{{end}}" bridge`
-    sudo firewall-cmd --permanent --new-zone minishift
-    sudo firewall-cmd --permanent --zone minishift --add-source $dockernet
-    sudo firewall-cmd --permanent --zone minishift --add-port 53/udp --add-port 8053/udp
-    sudo firewall-cmd --reload
-    read -p 'VM IP: ' serverip
-    minishift start --vm-driver generic --remote-ipaddress $serverip --remote-ssh-user $USER --remote-ssh-key $HOME/.ssh/id_rsa
-elif [[ "$platform" == 'MACOSX' ]]; then
-    echo "Installing Minishift on a MACOSX platform" 
-    brew cask install minishift
-
-    minishift addons install --defaults
-    minishift addons enable admin-user
-    minishift addon apply admin-user
-    minishift start
-fi
 
 
 echo "#################################################"
